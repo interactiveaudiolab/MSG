30b2f8d1e02636d4ca0589a810269288ba572df5
Noah Schaffer <noahschaffer2022@u.northwestern.edu>
2021-06-29 11:38:39 -0500

    Actually using skip connection properly

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ save_interval = 20
 log_interval = 100
 experiment_dir = 'saves_626_skip/'
 
-netG = GeneratorMel(n_mel_channels, ngf, n_residual_layers, skip_cxn=True).cuda()
+netG = GeneratorMel(n_mel_channels, ngf, n_residual_layers, skip_cxn=False).cuda()
 netD = DiscriminatorMel(
         num_D, ndf, n_layers_D, downsamp_factor
     ).cuda()

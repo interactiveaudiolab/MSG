e50c6f186fa35e9f62aceab5ecf4070fa4252f3a
boazcogan <boazcogan@yahoo.com>
2021-08-09 17:29:56 -0500

    Adjusting device to be based on config initialization, adding multi-gpu for training

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import soundfile as sf
 import matplotlib.pyplot as plt
 import imageio
 
-
+import torch.nn as nn
 
 from models.MelGAN import Audio2Mel, GeneratorMel, DiscriminatorMel, SISDRLoss
 from datasets.WaveDataset import MusicDataset
@@ -22,7 +22,7 @@ from datasets.Wrapper import DatasetWrapper
 
 
 pattern = re.compile('[\W_]+')
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = ""
 
 def parse_args(args_list):
     arg_names = [pattern.sub('', s) for s in args_list[::2]]
@@ -166,16 +166,20 @@ def main():
 
     wandb.init(config=params)
     config = wandb.config
-
+    
+    global device
+    device = torch.device(f"cuda:{config.gpus[0]}" if torch.cuda.is_available() else "cpu")
     np.random.seed(config.random_seed)
     torch.manual_seed(config.random_seed)
     
     netG = GeneratorMel(
         config.n_mel_channels, config.ngf, config.n_residual_layers,config.skip_cxn
         ).to(device)
+    netG = nn.DataParallel(netG, device_ids=config.gpus)
     netD = DiscriminatorMel(
             config.num_D, config.ndf, config.n_layers_D, config.downsamp_factor
         ).to(device)
+    netD = nn.DataParallel(netD, device_ids=config.gpus)
     fft = Audio2Mel(n_mel_channels=config.n_mel_channels).to(device)
 
     optG = torch.optim.Adam(netG.parameters(), lr=config.lr, betas=(config.b1,config.b2))

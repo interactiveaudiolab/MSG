d6b353aba5fc219ade1a6526a8692bac165cd18a
noahschaffer <noahschaffer2022@u.northwestern.edu>
2022-12-06 12:08:48 -0600

    add inference scrpt + update requirements.txt

---
+++ Run_Inference.py
@@ -0,0 +1,52 @@
+"""
+Run MSG Inference for a single audio example.
+E.g: python Run_Inference.py --audio_file=/Users/boazcogan/Northwestern/misc/model_saves/bass.wav --generator=/Users/boazcogan/Northwestern/misc/model_saves/29netG.pt
+"""
+import argparse
+import librosa
+import numpy as np
+import nussl
+from models.Demucs import *
+import os
+import torch
+import soundfile as sf
+
+from data_generation.generate_test_set import overlap_add_process
+
+def parseConfig(config):
+    exp_dict = yaml.load(open(os.path.join(config), 'r'),
+                         Loader=yaml.FullLoader)
+    return Struct(**exp_dict).parameters
+
+def run_inference(audio_example, generator_checkpoint, config=None):
+    device = torch.device(f"cuda:0" if torch.cuda.is_available() else "cpu")
+    sample_rate = 16_000
+    # load the song into a overlapping chunk vector
+    data, sr = librosa.load(audio_example, sr=sample_rate)
+    # data = data[10*16_000:16*16_000]
+    # now we have a list of shape [[n second clip], [n second clip]...]
+    netG = Demucs([""],audio_channels=1, samplerate=sample_rate ,segment_length=16_000, skip_cxn = True,lstm_layers=0, normalize=True).to(device)
+    netG.load_state_dict(torch.load(generator_checkpoint))
+    netG.eval()
+    
+    with torch.no_grad():
+        estimation = overlap_add_process(data, sample_rate, netG)
+    generated_signal = nussl.AudioSignal(audio_data_array=estimation)
+    
+    # perform inference
+    if not os.path.exists("msg_output"):
+        os.mkdir("msg_output")
+
+    # write audio
+    song_name = audio_example.split('/')[-1]
+    sf.write(f'msg_output/{song_name}', generated_signal.peak_normalize().audio_data.T, sample_rate)
+
+if __name__ == '__main__':
+    # Given a config, checkpoint, and song, run inference on the song
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--audio_file", "-a", type=str,
+                        help="absolute path to the audio file", required=True)
+    parser.add_argument("--generator", "-g", type=str, required=True,
+                        help="absolute path to the generator checkpoint")
+    exp, exp_args = parser.parse_known_args()
+    run_inference(exp.audio_file, exp.generator)
